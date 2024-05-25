20ca4a07844f9f270bb22dca945125ba645dad28
nodarisvanidze88 <nodarisvanidze88@gmail.com>
2024-05-25 18:05:25 +0400

    check 50 arithmetic

--- arithmetic/__init__.py
+++ arithmetic/__init__.py
@@ -7,11 +7,17 @@ def exists():
     check50.exists("arithmetic.py")
 
 @check50.check(exists)
-def testgiorgi():
-    """input of Giorgi yields output of 'Hello, Giorgi, nice to meet you!'"""
-    check50.run("python3 arithmetic.py").stdin("Giorgi", prompt=True).stdout("Hello, Giorgi, nice to meet you!").exit()
+def testSample():
+    """input of 5 and 5 yields output of 'Sum: 10 Difference: 0 Product: 25 Quotient: 1.0'"""
+    check50.run("python3 arithmetic.py")\
+        .stdin("5", prompt=True)\
+        .stdin("5", prompt=True)\
+        .stdout("Sum: 10\nDifference: 0\nProduct: 25\nQuotient: 1.0").exit()
 
 @check50.check(exists)
-def testalgouni():
-    """input of algouni yields output of 'Hello, algouni, nice to meet you!'"""
-    check50.run("python3 arithmetic.py").stdin("Algouni", prompt=True).stdout("Hello, Algouni, nice to meet you!").exit()
+def testSample():
+    """input of 5 and 5 yields output of 'Sum: 10 Difference: 0 Product: 25 Quotient: 1.0'"""
+    check50.run("python3 arithmetic.py")\
+        .stdin("12", prompt=True)\
+        .stdin("6", prompt=True)\
+        .stdout("Sum: 18\nDifference: 6\nProduct: 72\nQuotient: 2.0").exit()
