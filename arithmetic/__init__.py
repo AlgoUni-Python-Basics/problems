8d119a2534e6d229af227a58ee236ea0e5664ef2
nodarisvanidze88 <nodarisvanidze88@gmail.com>
2024-05-25 18:08:13 +0400

    check 50 arithmetic

--- arithmetic/__init__.py
+++ arithmetic/__init__.py
@@ -16,7 +16,7 @@ def testSample():
 
 @check50.check(exists)
 def testSample2():
-    """input of 5 and 5 yields output of 'Sum: 10 Difference: 0 Product: 25 Quotient: 1.0'"""
+    """input of 5 and 5 yields output of 'Sum: 18 Difference: 6 Product: 72 Quotient: 2.0'"""
     check50.run("python3 arithmetic.py")\
         .stdin("12", prompt=True)\
         .stdin("6", prompt=True)\
